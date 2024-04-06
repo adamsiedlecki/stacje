432f053c0a00a06dea0530f8abe1a8fa42999005
adamsiedlecki <adamsiedlec@gmail.com>
2024-04-06 09:32:05 +0200

    integer input

--- src/stacje.py
+++ src/stacje.py
@@ -6,6 +6,7 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from imageDownload import download_image
 
+
 class SayHello(App):
     def build(self):
         self.window = GridLayout()
@@ -15,7 +16,23 @@ class SayHello(App):
         image_downloaded = download_image('https://otm.asiedlecki.net/img/logo500.png')
         self.window.add_widget(Image(texture=image_downloaded.texture))
 
+        self.window.add_widget(Label(text='Witaj w tej wspaniałej aplikacji!'))
+        self.window.add_widget(Label(text='Test'))
+
+        self.user_input = TextInput(multiline=False)
+        self.user_input.input_filter='int'
+        self.window.add_widget(self.user_input)
+
+        self.button = Button(text="Wprowadź dane")
+        self.button.bind(on_press=self.button_callback)
+        self.window.add_widget(self.button)
+
         return self.window
+
+    def button_callback(self, instance):
+        text = self.user_input.text
+        print(text)
+
 
 if __name__ == "__main__":
     SayHello().run()
